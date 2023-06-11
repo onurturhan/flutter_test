237bd3cdc5fe099756b90346d24d227a6ace5b66
Onur Turhan <onurturhan@gmail.com>
2023-06-11 21:25:15 +0300

    some confs updated.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 
 void main() {
+//  WidgetsFlutterBinding.ensureInitialized();
+//  await Firebase.initializeApp(
+//    options: DefaultFirebaseOptions.currentPlatform,
+//  ); 
   runApp(const MyApp());
 }
 
